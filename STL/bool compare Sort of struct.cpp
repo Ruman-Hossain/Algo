5a3f8cd856ded0a576edb03332df8eb9adc1b9ba
Ruman Hossain <ruman.cse.brur@gmail.com>
2017-11-24 23:37:41 +0600

    Create bool compare Sort of struct.cpp

---
+++ STL/bool compare Sort of struct.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct data{
+    char name[100];
+    int height,weight;
+    long long income;
+
+};
+bool compare( data a, data b ) {
+    if( a.income == b.income ) {
+        if( a.height == b.height ) {
+            if( a.weight == b.weight )
+              return strlen( a.name ) < strlen( b.name );
+            else return a.weight < b.weight;
+        }else return a.height > b.height;
+    }else return a.income > b.income;
+}
+int main()
+{
+    int n;
+    while(1){
+        cout<<"Enter the value of n : ";
+        cin>>n;
+        struct data p[n];
+
+        for(int i=0;i<n;i++){
+
+            cout<<i+1<<". ";
+            cout<<"Name : ";
+            cin>>p[i].name;
+
+            cout<<"\tHeight : ";
+            cin>>p[i].height;
+
+            cout<<"\tWeight : ";
+            cin>>p[i].weight;
+
+            cout<<"\tIncome : ";
+            cin>>p[i].income;
+
+            cout<<endl;
+        }
+
+        sort(p,p+n,compare); //SORTING FUNCTION CALLING TO SORT
+
+        for(int i=0;i<n;i++){
+            cout<<i+1<<". ";
+            cout<<"Name : "<<p[i].name<<endl;
+            cout<<"\tHeight : "<<p[i].height<<endl;
+            cout<<"\tWeight : "<<p[i].weight<<endl;
+            cout<<"\tIncome : "<<p[i].income<<endl;
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}
